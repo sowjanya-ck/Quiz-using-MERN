959b2ba493b511648c59e3afb2da81262d3fd952
sowjanya-ck <65709982+sowjanya-ck@users.noreply.github.com>
2020-12-08 21:38:56 +0530

    App

---
+++ App.js
@@ -0,0 +1,33 @@
+/*import React from "react"
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import details from "./components/details";
+
+function App() {
+  return (
+  <div className = 'App'>
+  <Details />
+  </div>
+  );
+
+}
+
+export default App;
+*/
+import React from 'react';
+import './App.css';
+import Form from './components/Form';
+//import details from "./components/details";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Form />
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
